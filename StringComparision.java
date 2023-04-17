61bbccddc64770cc2c57165e7559934947d1b40b
Gaurav Sai Teja <128503039+21R01A7263@users.noreply.github.com>
2023-04-17 21:11:53 +0530

    Week2B

---
+++ StringComparision.java
@@ -0,0 +1,9 @@
+class stringcomp {
+    public static void main(String args[])
+    {
+        String s1= ("Hello");
+        String s2= ("Hello");
+        System.out.println(s1==s2);
+        System.out.println(s1.equals(s2));
+    }
+}
