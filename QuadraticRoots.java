9fe20b341efce742627d2ee64968a2510a25a34d
Gaurav Sai Teja <128503039+21R01A7263@users.noreply.github.com>
2023-04-17 21:10:45 +0530

    Week1A

---
+++ QuadraticRoots.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Quadratic{
+    public static void main(String[] args){
+        int a,b,c;
+        double root1,root2;
+        Scanner input =  new Scanner(System.in);
+        a=input.nextInt();
+        b=input.nextInt();
+        c=input.nextInt();
+        double det=(b*b) - (4*a*c);
+        System.out.println("Determinent is "+det);
+        if(det>0)
+        {
+            System.out.println("Roots are real and distinct");
+        }
+        else if(det==0)
+        {
+            System.out.println("Roots are real and equal");
+        }
+        else
+        {
+            System.out.println("Roots are imaginary");
+        }
+    }
+}
