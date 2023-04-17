5a9887c20d64345cde616077de850c4b32ee8bff
Gaurav Sai Teja <128503039+21R01A7263@users.noreply.github.com>
2023-04-17 21:11:22 +0530

    Week1B

---
+++ PrimeNumbers.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+class Primenum {
+    public static void main(String[] args)
+    {
+        int n,p,i,j;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the range for prime numbers");
+        n=input.nextInt();
+        if(n==0 || n==1)
+        {
+            System.out.println("No prime numbers");
+        }
+        else{
+            for(i=2;i<=n;i++)
+            {
+                p=0;
+                for(j=2;j<=i/2;j++)
+                {
+                    if(i%j==0)
+                    {
+                        p=1;
+                    }
+                }
+                if(p==0)
+                {
+                    System.out.println(i);
+                }
+            }
+        }
+    }
+    
+}
