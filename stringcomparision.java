3cde0434cf4561a4a20d8230774754a07fcfe679
Gaurav Sai Teja <128503039+21R01A7263@users.noreply.github.com>
2023-04-17 21:09:45 +0530

    Delete stringcomparision.java

--- stringcomparision.java
+++
@@ -1,9 +0,0 @@
-class Week2b {
-    public static void main(String args[])
-    {
-        String s1= ("Hello");
-        String s2= ("Hello");
-        System.out.println(s1==s2);
-        System.out.println(s1.equals(s2));
-    }
-}
