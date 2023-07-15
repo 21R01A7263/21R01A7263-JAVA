5914b780dd5fe79df65cf9a57e40dcdb58b797dd
Gaurav Sai Teja <128503039+21R01A7263@users.noreply.github.com>
2023-07-15 10:27:07 +0530

    Week10B

---
+++ SimpleCalc.java
@@ -0,0 +1,83 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.applet.*;
+/*	
+<applet code="Calc" height=300 width=300>
+</applet>
+*/
+public class SimpleCalc extends Applet implements ActionListener
+{
+    TextField tf;
+	double arg=0;
+	String op="=";
+	boolean start=true;
+	public void init()
+	{
+		setLayout(new BorderLayout());
+		tf=new TextField("0");
+		add(tf,BorderLayout.NORTH);
+		Panel p=new Panel();
+		p.setLayout(new GridLayout(4,4));
+		String buttons="123/456*789-0.+=";
+		for(int i=0;i<buttons.length();i++)
+		{
+			Button b=new Button(buttons.substring(i,i+1));
+			p.add(b);
+			b.addActionListener(this);
+		}
+		add(p);
+	}
+	public void actionPerformed(ActionEvent ae)
+	{
+		String s=ae.getActionCommand();
+		if('0'<=s.charAt(0)&&s.charAt(0)<='9'||s.equals("."))
+		{
+			if(start)
+			tf.setText(s);
+			else
+			tf.setText(tf.getText()+s);
+			start=false;
+		}
+		else
+		{
+			calcu(Double.parseDouble(tf.getText()));
+			op=s;
+			start=true;
+		}	
+	}
+	public void calcu(double n)
+	{
+		if(op.equals("+"))
+		arg+=n;
+		else
+		if(op.equals("-"))
+		arg-=n;
+		else
+                if(op.equals("*"))
+		arg*=n;
+		else
+		if(op.equals("/"))
+		{
+		try{
+		        arg/=n;
+		    }
+		   catch(ArithmeticException e)
+		  {
+		   tf.setText("Arithmetic Exception");
+		  }
+	 	}
+		else
+		if(op.equals("="))
+		arg=n;
+		tf.setText(""+arg);
+	}}
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        setLayout(new java.awt.BorderLayout());
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+}
